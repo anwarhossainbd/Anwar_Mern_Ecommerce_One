e0d1e45228666a695049b600c585515e895baa00
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-03-30 05:03:24 -0500

    Order Part Complete

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 const express =require("express");
-const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders,getAllOrders,updateOrder,deleteOrder } = require("../controllers/orderController");
 
 const router =express.Router();
 
@@ -11,4 +11,57 @@ router.route("/order/:id").get(isAuthenticatedUser,getSingleOrder);
 
 router.route("/orders/me").get(isAuthenticatedUser,myOrders)
 
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+
 module.exports =router
+
+
+
+
+
+
+
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+  
+    if (!order) {
+      return next(new ErrorHander("Order not found with this Id", 404));
+    }
+  
+    if (order.orderStatus === "Delivered") {
+      return next(new ErrorHander("You have already delivered this order", 400));
+    }
+  
+    if (req.body.status === "Shipped") {
+      order.orderItems.forEach(async (o) => {
+        await updateStock(o.product, o.quantity);
+      });
+    }
+    order.orderStatus = req.body.status;
+  
+    if (req.body.status === "Delivered") {
+      order.deliveredAt = Date.now();
+    }
+  
+    await order.save({ validateBeforeSave: false });
+    res.status(200).json({
+      success: true,
+    });
+  });
+  
+  async function updateStock(id, quantity) {
+    const product = await Product.findById(id);
+  
+    product.Stock -= quantity;
+  
+    await product.save({ validateBeforeSave: false });
+  }
+
